4cdbdbc98211d44b04bfa93fbd8ca67e6b2d87e7
paulwrath1223 <paul@fornage.net>
2021-10-02 15:10:37 -0700

    added % done indicator

--- paul_fuckery.py
+++ paul_fuckery.py
@@ -13,8 +13,10 @@ def change_hue(
     image = Image.open(image_path)
     pixels = image.load()
     hue_adjust = new_color_hsl[0] - old_color_hsl[0]
+    last_percent_done = 0
 
     for i in range(image.size[0]):  # for every col:
+        percent_done = int((i/image.size[0])*100)
         for j in range(image.size[1]):  # For every row
             # if pixels[i, j][3] == 255:  # If pixel is not transparent
             pixel_rgb = pixels[i, j]
@@ -31,10 +33,15 @@ def change_hue(
                     int(255 * new_color[0]), int(255 * new_color[1]), int(255 * new_color[2]), transparency)
 
                 pixels[i, j] = new_color_with_transparency
+
+        if last_percent_done != percent_done:
+            print(f"{percent_done}% complete")
+            last_percent_done = percent_done
     image.save(new_image_path)
 
 
 # img_path = r"C:\Users\paulw\PycharmProjects\RecolorImage\new_img1.png"
-img_path = r"C:\Users\paulw\PycharmProjects\RecolorImage\Header-Benefits-of-Big-Trees.png"
+# img_path = r"C:\Users\paulw\PycharmProjects\RecolorImage\Header-Benefits-of-Big-Trees.png"
+img_path = r"C:\Users\paulw\PycharmProjects\RecolorImage\camera_sample.png"
 new_img_path = r"C:\Users\paulw\PycharmProjects\RecolorImage\pauled_img1.png"
-change_hue(img_path, new_img_path, (157, 172, 35), blue, 0.04)
+change_hue(img_path, new_img_path, (198, 192, 178), blue, 0.04)
