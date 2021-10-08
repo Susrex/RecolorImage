baa7a245cbf5671fa3edaa1cf60565c059346af4
paulwrath1223 <paul@fornage.net>
2021-10-08 13:20:48 -0700

    ~working

--- color range selector.py
+++ color range selector.py
@@ -6,9 +6,8 @@ blue = (82, 147, 157)
 
 class ColorRange:
 
-    def __init__(self, whitelistColors, blacklistColors, tol=10):
+    def __init__(self, whitelistColors, blacklistColors):
         self.colors = []
-        self.tolerance = tol
         for c1 in whitelistColors:
             self.colors.append(c1)
         for c2 in blacklistColors:
@@ -27,16 +26,12 @@ class ColorRange:
             except ValueError:
                 pass
 
-    def check(self, color):
+    def closest_mark(self, color):
         closestMark = self.colors[0]
         for c in self.colors:
             if color - abs(c) < color - abs(closestMark):
                 closestMark = c
-        if closestMark <= 0:  # if the closest mark is a blacklist, do not change.
-            return False
-        if color - closestMark < self.tolerance:
-            return True
-        return False
+        return closestMark
 
 
 def change_hue(
@@ -51,13 +46,17 @@ def change_hue(
     for col in blacklist_colors_rgb:
         blacklist_colors_hue.append(colorsys.rgb_to_hls(col[0] / 255, col[1] / 255, col[2] / 255)[0])
 
-    cr = ColorRange(whitelist_colors_hue, blacklist_colors_hue, hue_tolerance)
+    cr = ColorRange(whitelist_colors_hue, blacklist_colors_hue)
     cr.removeDuplicates()
 
     new_color_hsl = colorsys.rgb_to_hls(new_color_rgb[0]/255, new_color_rgb[1]/255, new_color_rgb[2]/255)
     image = Image.open(image_path)
     pixels = image.load()
-    hue_adjust = new_color_hsl[0] - old_color_hsl[0]
+    whitelistHueSum = 0
+    for color in whitelist_colors_hue:
+        whitelistHueSum += color
+    avgWhitelistHue = whitelistHueSum/len(whitelist_colors_hue)
+    hue_adjust = new_color_hsl[0] - avgWhitelistHue
     last_percent_done = 0
 
     for i in range(image.size[0]):  # for every col:
@@ -70,7 +69,7 @@ def change_hue(
             except:
                 transparency = 255
             pixel_hsl = colorsys.rgb_to_hls(pixel_rgb[0]/255, pixel_rgb[1]/255, pixel_rgb[2]/255)
-            hue_offset_to_target = pixel_hsl[0] - old_color_hsl[0]
+            hue_offset_to_target = pixel_hsl[0] - cr.closest_mark(pixel_hsl[0])
             if abs(hue_offset_to_target) <= hue_tolerance:  # if color should be replaced
                 pixel_hsl = ((pixel_hsl[0]+hue_adjust), pixel_hsl[1], pixel_hsl[2])
                 new_color = colorsys.hls_to_rgb(pixel_hsl[0], pixel_hsl[1], pixel_hsl[2])
@@ -86,7 +85,8 @@ def change_hue(
 
 
 # img_path = r"C:\Users\paulw\PycharmProjects\RecolorImage\new_img1.png"
-# img_path = r"C:\Users\paulw\PycharmProjects\RecolorImage\Header-Benefits-of-Big-Trees.png"
-img_path = r"C:\Users\paulw\PycharmProjects\RecolorImage\camera_sample.png"
+img_path = r"C:\Users\paulw\PycharmProjects\RecolorImage\Header-Benefits-of-Big-Trees.png"
+# img_path = r"C:\Users\paulw\PycharmProjects\RecolorImage\camera_sample.png"
 new_img_path = r"C:\Users\paulw\PycharmProjects\RecolorImage\pauled_img1.png"
-change_hue(img_path, new_img_path, (198, 192, 178), blue, 10)
+change_hue(img_path, new_img_path,
+           [(198, 192, 178), (248, 243, 1)], [(70, 60, 3), (43, 37, 2), (125, 103, 20)], blue, 0.04)
